36795c79a7d63439aa0dc08fa6288295cdf6c89a
muxin-web3 <muxin333333@gmail.com>
2022-06-26 13:02:46 +1200

    feat introduction video section

--- sections/SectionVideo.js
+++ sections/SectionVideo.js
@@ -1,7 +1,20 @@
+import React from 'react';
 import { t } from '@lingui/macro';
 
 import Container from '@/components/Container';
 
-export default function SectionVideo({ id, bgcolor }) {
-  return <Container title={t`lxdao-intro-video-title`}>sfasdf</Container>;
-}
+const SectionVideo = () => (
+  <Container title={t`lxdao-intro-video-title`}>
+    <iframe
+      width="750"
+      height="440"
+      src="https://www.youtube.com/embed/BshxCIjNEjY"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen={true}
+    ></iframe>
+  </Container>
+);
+
+export default SectionVideo;
