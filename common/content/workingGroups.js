7a06766b984273f910b36ee025193c8daf873a93
muxin-web3 <muxin333333@gmail.com>
2022-10-24 11:44:24 +0800

    fix: moved the working group card component into the components folder for reusing

---
+++ common/content/workingGroups.js
@@ -0,0 +1,24 @@
+const workingGroupsData = [
+  {
+    image: '/images/workingGroups/governance-group.svg',
+    name: 'Governance Group',
+    link: 'https://www.notion.so/lxdao/Governance-Group-a3886d112d3b45c8a4fad6c178e2846b',
+  },
+  {
+    image: '/images/workingGroups/onboarding-committee.svg',
+    name: 'Onboarding Committee',
+    link: 'https://www.notion.so/lxdao/Onboarding-Committee-680a0a9e034d482db273f24760d93f83',
+  },
+  {
+    image: '/images/workingGroups/LXDAO-tools.svg',
+    name: 'LXDAO Tools',
+    link: 'https://www.notion.so/lxdao/LXDAO-Tools-5425810ff08e42d9913d74a2a883d9c2',
+  },
+  {
+    image: '/images/workingGroups/LXDAO-dataCenter.svg',
+    name: 'LXDAO DataCenter',
+    link: 'https://www.notion.so/lxdao/LXDAO-DataCenter-40dfb4928f734c8eab0919490881d013',
+  },
+];
+
+export default workingGroupsData;
