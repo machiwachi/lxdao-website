f569a5873f827afcbcbbc76641b062195037487f
Bruce Xu <bruce.xu@lxdao.io>
2022-11-04 23:14:09 +0800

    feat: add event page

---
+++ pages/events.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+import React from 'react';
+
+export default function Events() {
+  return (
+    <div>
+      <iframe
+        src="https://outlook.office365.com/calendar/published/d831b3ca3fac44ef82097e29f76ad7fa@lxdao.io/ec1afe2db1b747e895a79ccff77fe85d7022880942165855388/calendar.html"
+        style={{
+          border: '0px #ffffff none',
+        }}
+        name="myiFrame"
+        scrolling="no"
+        frameborder="1"
+        marginheight="0px"
+        marginwidth="0px"
+        height="1000px"
+        width="100%"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
