6f5ccf0f796cf9095c4fa42ff2ff8ebdce28d9bd
Bruce Xu <bruce.xu@lxdao.io>
2022-11-04 23:20:40 +0800

    feat: adjust height

--- pages/events.js
+++ pages/events.js
@@ -9,14 +9,9 @@ export default function Events() {
         style={{
           border: '0px #ffffff none',
         }}
-        name="myiFrame"
-        scrolling="no"
         frameborder="1"
-        marginheight="0px"
-        marginwidth="0px"
-        height="1000px"
         width="100%"
-        allowfullscreen
+        height="900px"
       ></iframe>
     </div>
   );
