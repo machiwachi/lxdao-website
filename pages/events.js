375663ddfb27948d1d16ccaecdadd455b6c6354e
Bruce Xu <bruce.xu@lxdao.io>
2022-11-05 17:05:48 +1300

    feat: update with UTC0 timezone

--- pages/events.js
+++ pages/events.js
@@ -5,7 +5,7 @@ export default function Events() {
   return (
     <div>
       <iframe
-        src="https://outlook.office365.com/calendar/published/d831b3ca3fac44ef82097e29f76ad7fa@lxdao.io/ec1afe2db1b747e895a79ccff77fe85d7022880942165855388/calendar.html"
+        src="https://outlook.office365.com/calendar/published/d831b3ca3fac44ef82097e29f76ad7fa@lxdao.io/28ac6994386049198bf6eee4314f31525661633157997162338/calendar.html"
         style={{
           border: '0px #ffffff none',
         }}
