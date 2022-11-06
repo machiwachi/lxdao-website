c19f8dc970e150ff68f15b37e711f571563a783b
Jacky <sjyanxin@126.com>
2022-11-06 23:38:20 +0800

    feat: change join us style

--- sections/SectionApplicationSteps.js
+++ sections/SectionApplicationSteps.js
@@ -32,21 +32,32 @@ const steps = [
 ];
 
 const SectionApplicationSteps = () => {
-  const Step = ({ stepData }) => {
+  const Step = ({ stepData, index }) => {
     const { label, description, icon } = stepData;
     return (
-      <Box display="flex" flexDirection="column" alignItems="center" gap="12px">
+      <Box display="flex" flexDirection="column" alignItems="center" gap="16px">
         <Box
-          width="60px"
-          height="60px"
+          width="120px"
+          height="120px"
           display="flex"
           alignItems="center"
           justifyContent="center"
+          backgroundColor="rgba(16, 24, 40, 0.05)"
+          borderRadius="100px"
         >
-          <Box width="56px" component={'img'} src={icon} />
+          <Box width="42px" component={'img'} src={icon} />
         </Box>
-        <Typography variant="h5">{label}</Typography>
-        <Typography variant="body1">{description}</Typography>
+        <Typography fontWeight="500" color="#000" fontSize="20px">
+          {label}
+        </Typography>
+        <Typography
+          variant="body1"
+          paddingX={index === 2 ? 0 : 2}
+          lineHeight="30px"
+          color="#667085"
+        >
+          {description}
+        </Typography>
       </Box>
     );
   };
@@ -59,25 +70,32 @@ const SectionApplicationSteps = () => {
         margin="0 auto"
       >
         <Box display="flex" flexDirection="column" alignItems="center">
-          <Typography variant="h3" marginBottom={10} textAlign="center">
+          <Typography
+            textTransform="uppercase"
+            fontSize="48px"
+            fontWeight="600"
+            color="#101828"
+            marginBottom={10}
+            textAlign="center"
+          >
             How to become a LX Buidler
           </Typography>
           <Box
             display="flex"
             flexDirection={{ lg: 'row', xs: 'column' }}
             alignItems="center"
-            gap={{ lg: '35px', sm: '80px', xs: '30px' }}
+            gap={{ lg: '46px', sm: '80px', xs: '30px' }}
           >
             {steps.map((step, index) => {
               return (
                 <Box key={index} position="relative" flex="1 1 25%">
-                  <Step stepData={step} />
+                  <Step index={index} stepData={step} />
                   {index !== steps.length - 1 ? (
                     <>
                       <Box
                         position="absolute"
                         top="160px"
-                        left="185px"
+                        left="218px"
                         bottom="40px"
                         alignItems="center"
                         display={{ lg: 'none', sm: 'block', xs: 'none' }}
@@ -89,6 +107,9 @@ const SectionApplicationSteps = () => {
                           borderRight="1px dashed #101828"
                         />
                         <Box
+                          position="absolute"
+                          top="1px"
+                          right="0"
                           width="5px"
                           height="5px"
                           borderRadius="100%"
@@ -98,23 +119,23 @@ const SectionApplicationSteps = () => {
 
                       <Box
                         position="absolute"
-                        top="30px"
+                        top="55px"
                         left="185px"
                         bottom="40px"
                         alignItems="center"
                         display={{ lg: 'block', xs: 'none' }}
                       >
                         <Box
-                          width="140px"
-                          position="relative"
-                          top="3px"
-                          borderTop="1px dashed #101828"
-                        />
-                        <Box
                           width="5px"
                           height="5px"
                           borderRadius="100%"
                           backgroundColor="#000000"
+                        />
+                        <Box
+                          width="140px"
+                          position="relative"
+                          top="3px"
+                          borderTop="1px dashed #101828"
                         />
                       </Box>
                     </>
@@ -123,8 +144,13 @@ const SectionApplicationSteps = () => {
               );
             })}
           </Box>
-          <Link href="https://tally.so/r/3Ek62r" target="_blank">
-            <Button marginTop={8} width="200px">
+          <Link
+            sx={{ textDecoration: 'none' }}
+            textTransform="uppercase"
+            href="https://tally.so/r/3Ek62r"
+            target="_blank"
+          >
+            <Button variant="gradient" marginTop={10} width="260px">
               Start your Application
             </Button>
           </Link>
--- sections/SectionJoinUsHero.js
+++ sections/SectionJoinUsHero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import SectionHero from './SectionHero';
 
 const SectionJoinUsHero = () => {
@@ -7,71 +7,137 @@ const SectionJoinUsHero = () => {
     return (
       <>
         <Typography
-          variant="h3"
-          lineHeight="1.5em"
-          marginBottom={3}
+          fontSize={{ md: '98px', xs: '62px' }}
+          fontWeight="700"
+          marginBottom={6}
           textTransform="uppercase"
-          display={{ lg: 'block', xs: 'none' }}
+          textAlign="left"
+          color="#101828"
         >
-          Join the <strong>LXDAO</strong>
-          <br />
-          community
-        </Typography>
-        <Typography
-          variant="h3"
-          lineHeight="1.5em"
-          marginBottom={3}
-          textTransform="uppercase"
-          display={{ lg: 'none', xs: 'block' }}
-        >
-          Join the <strong>LXDAO</strong> community
+          JOIN US
         </Typography>
       </>
     );
   };
   return (
-    <SectionHero
-      leftContent={
+    <Box>
+      <SectionHero
+        leftContent={
+          <Box>
+            <Box width={{ sm: '592px', xs: '350px' }}>
+              <Title />
+              <Typography
+                variant="subtitle1"
+                lineHeight="36px"
+                color="#667085"
+                fontSize="21px"
+              >
+                LXDAO is formed by a group of Web3 buidlers who enjoy building
+                high-quality valuable Web3 products. We believe that in ten
+                years, there will be one billion people using Web3 technologies,
+                products and ideas every day. Therefore, as a pioneer, we will
+                use the Web3 approach to buidl and maintain projects to promote
+                the development of Web3 .
+              </Typography>
+              <Typography
+                marginTop={3}
+                variant="subtitle1"
+                lineHeight="36px"
+                color="#667085"
+                fontSize="21px"
+              >
+                Our outputs include but are not limited to commercial projects,
+                open source projects, public goods and performance art, etc. We
+                also want to create a sustainable circular economy so that LXDAO
+                can continue to operate.
+              </Typography>
+            </Box>
+          </Box>
+        }
+        rightContent={
+          <Box width={{ sm: '420px', xs: '300px' }}>
+            <img src="/images/to-the-moon.svg" width="100%" />
+          </Box>
+        }
+      />
+
+      <Box
+        width="100%"
+        minHeight={{ md: '400px', xs: '660px' }}
+        display="flex"
+        flexDirection={{ sm: 'row', xs: 'column' }}
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        gap={{ sm: '170px', xs: '40px' }}
+      >
+        <Box
+          padding="0 36px"
+          display="flex"
+          flexDirection="column"
+          alignItems={{ lg: 'flex-start', xs: 'center' }}
+          textAlign={{ lg: 'left', xs: 'center' }}
+        >
+          <Box width={{ sm: '400px', xs: '300px' }}>
+            <img src="/images/brainstorm.svg" width="100%" />
+          </Box>
+        </Box>
         <Box>
-          <Title />
-          <Typography
-            variant="body1"
-            lineHeight="1.5em"
-            fontSize={{ md: '24px', xs: '16px' }}
-          >
-            LXDAO is formed by a group of{' '}
-            <strong>
-              Web3 buidlers who enjoy building high-quality valuable Web3
-              products
-            </strong>
-            . We believe that in ten years, there will be one billion people
-            using Web3 technologies, products and ideas every day. Therefore, as
-            a pioneer,{' '}
-            <strong>
-              we will use the Web3 approach to buidl and maintain projects to
-              promote the development of Web3
-            </strong>
-            .
-          </Typography>{' '}
-          <Typography
-            marginTop={2}
-            variant="body1"
-            lineHeight="1.5em"
-            fontSize={{ md: '24px', xs: '16px' }}
-          >
-            Our outputs include but are not limited to commercial projects, open
-            source projects, public goods and performance art, etc. We also want
-            to <strong>create a sustainable circular economy</strong> so that
-            LXDAO can continue to operate.
-          </Typography>
+          <Box width="592px">
+            <Typography
+              variant="subtitle1"
+              lineHeight="36px"
+              color="#667085"
+              fontSize="21px"
+              textTransform="capitalize"
+            >
+              LXDAO wants to attract people who want to build conscientiously in
+              any role, whether you are a programmer, designer, artist,
+              operation or product manager, we welcome you to join us.
+            </Typography>
+          </Box>
         </Box>
-      }
-      rightContent={
-        <Box width={{ sm: '420px', xs: '300px' }}>
-          <img src="/images/join-us-img.png" width="100%" />
+      </Box>
+
+      <Box
+        width="100%"
+        minHeight={{ md: '362px', xs: '660px' }}
+        display="flex"
+        flexDirection={{ sm: 'row', xs: 'column' }}
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        gap={{ sm: '200px', xs: '40px' }}
+        marginBottom="107px"
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems={{ lg: 'flex-start', xs: 'center' }}
+          textAlign={{ lg: 'left', xs: 'center' }}
+        >
+          <Box width="532px">
+            <Typography
+              variant="subtitle1"
+              lineHeight="36px"
+              color="#667085"
+              fontSize="21px"
+              textTransform="capitalize"
+            >
+              In LXDAO you can buidl many web3 projects with like-minded
+              buidlers in the web3 way, you can not only increase your web3
+              experience and get paid with LXPoints,but also make a lot of good
+              friends.
+            </Typography>
+          </Box>
         </Box>
-      }
-    />
+        <Box>
+          <Box width={{ sm: '400px', xs: '300px' }}>
+            <img src="/images/build-project.svg" width="100%" />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
