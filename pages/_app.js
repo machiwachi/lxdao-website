b812a6e7aded9202ce8466d15bae35d12419f292
muxin-web3 <muxin333333@gmail.com>
2023-05-27 12:47:07 +1200

    Set up img3 global default gateways

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit';
 import { WagmiConfig } from 'wagmi';
+import { Img3Provider } from '@lxdao/img3';
 
 import getTheme from '@/common/theme';
 import { AlertProvider } from '@/context/AlertContext';
@@ -18,20 +19,28 @@ import '@/common/style.css';
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={getTheme('light')}>
-      <AlertProvider>
-        <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider
-            theme={lightTheme({
-              borderRadius: 'small',
-              accentColor: 'linear-gradient(90deg, #305FE8 0%, #3AD9E3 100%)',
-            })}
-            chains={chains}
-          >
-            <Component {...pageProps} />
-            <AlertPopup />
-          </RainbowKitProvider>
-        </WagmiConfig>
-      </AlertProvider>
+      <Img3Provider
+        defaultGateways={[
+          'https://nftstorage.link/ipfs/',
+          'https://ipfs-gateway.cloud/ipfs/',
+          'https://gateway.pinata.cloud/ipfs/',
+        ]}
+      >
+        <AlertProvider>
+          <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider
+              theme={lightTheme({
+                borderRadius: 'small',
+                accentColor: 'linear-gradient(90deg, #305FE8 0%, #3AD9E3 100%)',
+              })}
+              chains={chains}
+            >
+              <Component {...pageProps} />
+              <AlertPopup />
+            </RainbowKitProvider>
+          </WagmiConfig>
+        </AlertProvider>
+      </Img3Provider>
     </ThemeProvider>
   );
 }
