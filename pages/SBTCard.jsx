e4ac5f8dba5fe731eac63c58e9ceae0a0a5a739d
0xhardman <1142783160@qq.com>
2024-09-04 00:18:26 +0800

    fix: :bug: try fix can't mint

--- pages/SBTCard.jsx
+++ pages/SBTCard.jsx
@@ -40,6 +40,7 @@ export default function MintCard() {
   const [minting, setMinting] = React.useState(false);
   const [projects, setProjects] = React.useState([]);
   const [, record, , refresh] = useBuidler(address);
+  console.log({ record, address });
   const router = useRouter();
   const [workingGroupsData, setWorkingGroupsData] = useState([]);
   const { data: balance } = useBalance({
