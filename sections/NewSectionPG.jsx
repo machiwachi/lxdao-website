61ce0976315d5f1d933041f0de221aaa8a1222c7
0xhardman <1142783160@qq.com>
2024-10-18 22:37:49 +0800

    fix: :bug: fix pg on mobile

--- sections/NewSectionPG.jsx
+++ sections/NewSectionPG.jsx
@@ -9,23 +9,24 @@ export default function NewSectionPG() {
       <Box
         sx={{
           display: 'flex',
+          flexDirection: { md: 'row', xs: 'column' },
           position: 'relative',
           justifyContent: 'space-between',
           width: '100%',
-          height: '725px',
+          height: { md: '725px', xs: '100%' },
         }}
       >
         <Box
           sx={{
             display: 'flex',
             flexDirection: 'column',
-            justifyContent: 'space-between',
+            justifyContent: { md: 'space-between', xs: 'flex-start' },
           }}
         >
           <Typography
-            fontSize="48px"
+            fontSize={{ md: '48px', xs: '32px' }}
             fontWeight="800"
-            maxWidth="514px"
+            maxWidth={{ md: '514px', xs: '100%' }}
             lineHeight="140%"
           >
             Why the{' '}
@@ -37,31 +38,16 @@ export default function NewSectionPG() {
               Public Goods
             </span>{' '}
             is undervalued
-            <br /> and needs
-            <br /> our attention?
+            <br hidden /> and needs
+            <br hidden /> our attention?
           </Typography>
-          <Box
-            sx={{
-              width: '441px',
-              pt: '80px',
-            }}
-          >
-            <Typography fontWeight="700" fontSize="28px" color="#10D7C4">
-              Why
-            </Typography>
-            <Box
-              component="img"
-              mt="12px"
-              src="/images/new/Union.svg"
-              height="21px"
-            />
-            <Typography letterSpacing={'-0.32px'} lineHeight="160%" mt="48px">
-              In the Web3 context, public goods are resources that everyone can
+          <ReasonsBox
+            title="What"
+            description="In the Web3 context, public goods are resources that everyone can
               access and no one can be excluded from using. ShapeShift dApp
               exemplifies this through its decentralized, open-source software
-              that anyone can use, modify, and improve.
-            </Typography>
-          </Box>
+              that anyone can use, modify, and improve."
+          />
         </Box>
         <Box
           sx={{
@@ -70,57 +56,29 @@ export default function NewSectionPG() {
             justifyContent: 'end',
           }}
         >
-          <Box
-            sx={{
-              width: '441px',
-              pt: '100px',
-            }}
-          >
-            <Typography fontWeight="700" fontSize="28px" color="#10D7C4">
-              What
-            </Typography>
-            <Box
-              component="img"
-              mt="12px"
-              src="/images/new/Union.svg"
-              height="21px"
-            />
-            <Typography letterSpacing={'-0.32px'} lineHeight="160%" mt="48px">
-              In the Web3 context, public goods are resources that everyone can
+          <ReasonsBox
+            title="What"
+            description="In the Web3 context, public goods are resources that everyone can
               access and no one can be excluded from using. ShapeShift dApp
               exemplifies this through its decentralized, open-source software
-              that anyone can use, modify, and improve.
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              width: '441px',
-              pt: '80px',
-            }}
-          >
-            <Typography fontWeight="700" fontSize="28px" color="#10D7C4">
-              How
-            </Typography>
-            <Box
-              component="img"
-              mt="12px"
-              src="/images/new/Union.svg"
-              height="21px"
-            />
-            <Typography letterSpacing={'-0.32px'} lineHeight="160%" mt="48px">
-              In the Web3 context, public goods are resources that everyone can
+              that anyone can use, modify, and improve."
+          />
+
+          <ReasonsBox
+            title="How"
+            description="In the Web3 context, public goods are resources that everyone can
               access and no one can be excluded from using. ShapeShift dApp
               exemplifies this through its decentralized, open-source software
-              that anyone can use, modify, and improve.
-            </Typography>
-          </Box>
+              that anyone can use, modify, and improve."
+          />
         </Box>
       </Box>
       <Box
         sx={{
           display: 'flex',
+          flexDirection: { md: 'row', xs: 'column' },
           justifyContent: 'space-between',
-          mt: '140px',
+          mt: { md: '140px', xs: '80px' },
         }}
       >
         <Box
@@ -128,51 +86,98 @@ export default function NewSectionPG() {
             display: 'flex',
             position: 'relative',
             flexDirection: 'column',
-            gap: '100px',
+
             justifyContent: 'space-between',
+            alignItems: 'center',
             px: '30px',
           }}
         >
-          <Box
-            width="200px"
-            height="22px"
-            bgcolor="#10D7C4"
-            sx={{
-              position: 'absolute',
-              left: '-200px',
-              top: '10px',
-            }}
-          ></Box>
-          <Typography fontSize="28px" fontWeight="700">
+          <Typography fontSize={{ md: '28px', xs: '18px' }} fontWeight="700">
             Learn more in our report
           </Typography>
-          <Button width="210px" borderRadius="100px" variant="gradient">
+          <Box
+            width="200px"
+            height={{ md: '22px', xs: '11px' }}
+            bgcolor="#10D7C4"
+            sx={{
+              position: { md: 'absolute', xs: 'relative' },
+              left: { md: '-200px', xs: '0' },
+              top: { md: '10px', xs: '0' },
+            }}
+          ></Box>
+
+          <Button
+            width={{ md: '210px', xs: '180px' }}
+            borderRadius="100px"
+            variant="gradient"
+            mt={{ md: '100px', xs: '40px' }}
+            fontSize={{ md: '16px', xs: '14px' }}
+          >
             DOWNLOAD PDF
           </Button>
         </Box>
-        <Box ml="100px">
+        <Box
+          ml={{ md: '100px', xs: '0' }}
+          mt={{ md: '0', xs: '40px' }}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
           <Box
             sx={{
               position: 'relative',
-              width: '320px',
-              height: '320px',
+              width: { md: '320px', xs: '280px' },
+              height: { md: '320px', xs: '280px' },
               bgcolor: '#10D7C4',
               borderRadius: '100%',
-              mr: '200px',
+              mr: { md: '200px', xs: '0' },
             }}
           >
             <Box
               component="img"
               src="/images/new/report.png"
               sx={{
-                mt: '60px',
-                ml: '80px',
-                height: '196px',
+                mt: { md: '60px', xs: '70px' },
+                ml: { md: '80px', xs: '-10px' },
+                height: { md: '196px', xs: '130px' },
               }}
             />
           </Box>
         </Box>
       </Box>
     </Container>
+  );
+}
+
+function ReasonsBox({ title, description }) {
+  return (
+    <Box
+      sx={{
+        width: { md: '441px', xs: '100%' },
+        pt: { md: '100px', xs: '20px' },
+      }}
+    >
+      <Typography
+        fontWeight="700"
+        fontSize={{ md: '28px', xs: '20px' }}
+        color="#10D7C4"
+      >
+        {title}
+      </Typography>
+      <Box
+        component="img"
+        mt={{ md: '12px', xs: '18px' }}
+        src="/images/new/Union.svg"
+        height={{ md: '21px', xs: '11px' }}
+      />
+      <Typography
+        letterSpacing={'-0.32px'}
+        lineHeight="160%"
+        fontSize={{ md: '16px', xs: '14px' }}
+        mt={{ md: '48px', xs: '10px' }}
+      >
+        {description}
+      </Typography>
+    </Box>
   );
 }
