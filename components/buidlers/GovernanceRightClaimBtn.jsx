092299dc8536da39cc488c76fc8932c5d6589fd8
0xhardman <1142783160@qq.com>
2024-12-08 20:11:22 +0800

    fix

--- components/buidlers/GovernanceRightClaimBtn.jsx
+++ components/buidlers/GovernanceRightClaimBtn.jsx
@@ -1,38 +1,43 @@
+import { use, useState } from 'react';
+
 import Button from '@/components/Button';
 import showMessage from '@/components/showMessage';
 
 import { waitForTransactionReceipt } from 'viem/actions';
 
-import { useAccount, useReadContract, useWriteContract } from 'wagmi';
+import {
+  useAccount,
+  useClient,
+  useReadContract,
+  useWriteContract,
+} from 'wagmi';
 
 import { voteContract } from 'abi';
 
 export default function GovernanceRightClaimBtn({ record }) {
   const { address } = useAccount();
-  const {
-    data: currentSeason,
-    error,
-    isLoading,
-  } = useReadContract({
+  const client = useClient();
+  const [loading, setLoading] = useState(false);
+  const { data: currentSeason, error } = useReadContract({
     ...voteContract,
     functionName: 'currentSeason',
     args: [],
   });
-  console.log({ currentSeason, error, isLoading });
   const { data: balanceOf } = useReadContract({
     ...voteContract,
     functionName: 'balanceOf',
     args: [address, currentSeason],
   });
-  console.log({ balanceOf, currentSeason });
   const { writeContractAsync, isError } = useWriteContract();
   const handleClaim = async () => {
     try {
+      setLoading(true);
       const tx = await writeContractAsync({
         ...voteContract,
         functionName: 'mint',
       });
-      const res = await waitForTransactionReceipt(tx);
+      console.log({ tx });
+      const res = await waitForTransactionReceipt(client, { hash: tx });
       showMessage({
         type: 'success',
         title: 'Governance Right Claimed',
@@ -44,12 +49,15 @@ export default function GovernanceRightClaimBtn({ record }) {
         title: 'Failed to claim governance right',
         body: error.message,
       });
+    } finally {
+      setLoading(false);
     }
   };
   const claimed = balanceOf > 0;
   if (record.address != address) {
     return null;
   }
+
   return (
     <Button
       variant="outlined"
@@ -57,11 +65,13 @@ export default function GovernanceRightClaimBtn({ record }) {
         textAlign: 'center',
       }}
       onClick={handleClaim}
-      disabled={claimed}
+      disabled={claimed || loading}
     >
-      {claimed
-        ? `Governance Right Claimed In S${currentSeason?.toString()}`
-        : `Claim Governance Right In S${currentSeason?.toString()}`}
+      {loading
+        ? 'Please Wait...'
+        : claimed
+          ? `Governance Right Claimed In S${currentSeason?.toString()}`
+          : `Claim Governance Right In S${currentSeason?.toString()}`}
     </Button>
   );
 }
